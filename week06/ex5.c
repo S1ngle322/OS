c4cab72d932469c3c684247b4c15586578a928a9
Daniil Sinelnik <45969897+S1ngle322@users.noreply.github.com>
2020-09-22 13:24:27 +0300

    Add files via upload

---
+++ week06/ex5.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+void parent(int child_pid) {
+    sleep(10);
+
+    kill(child_pid, SIGTERM);
+}
+
+void child1() {
+    while (1) {
+        printf("I'm alive\n");
+        fflush(stdout);
+
+        sleep(1);
+    }
+}
+
+int main() {
+    int p = fork();
+
+    if (p > 0) {
+        parent(p);
+    } else {
+        child1();
+    }
+}
