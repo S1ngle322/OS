375a8746713ca81853a969e3d4aba50c1efbd99d
Daniil Sinelnik <45969897+S1ngle322@users.noreply.github.com>
2020-10-27 13:36:36 +0300

    Add files via upload

---
+++ week11/ex2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+    char buff[5];
+
+    setvbuf(stdin, buff, _IOLBF, sizeof buff);
+
+    char * string = "Hello";
+
+    for (int i = 0; i < 5; ++i) {
+        printf("%c", string[i]);
+
+        sleep(1);
+    }
+}
